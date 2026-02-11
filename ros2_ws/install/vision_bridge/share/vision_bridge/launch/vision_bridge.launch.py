b7be0b2fbf2dc0e879b20e1dfda0cfba6891f043
Pierre Ishak <piroomira@live.com>
2026-02-11 15:49:46 -0500

    Intial commit

---
+++ ros2_ws/install/vision_bridge/share/vision_bridge/launch/vision_bridge.launch.py
@@ -0,0 +1,25 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+
+def generate_launch_description():
+
+    use_vicon = DeclareLaunchArgument('use_vicon', default_value='true')
+    use_realsense = DeclareLaunchArgument('use_realsense', default_value='false')
+
+    return LaunchDescription([
+        use_vicon,
+        use_realsense,
+
+        Node(
+            package='vision_bridge',
+            executable='vision_bridge',
+            name='vision_bridge_node',
+            output='screen',
+            parameters=[
+                {'use_vicon': LaunchConfiguration('use_vicon')},
+                {'use_realsense': LaunchConfiguration('use_realsense')}
+            ],
+        ),
+    ])
---
+++ ros2_ws/src/flight_2/flight_2/my_node.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from flight_2.')
+
+
+if __name__ == '__main__':
+    main()
